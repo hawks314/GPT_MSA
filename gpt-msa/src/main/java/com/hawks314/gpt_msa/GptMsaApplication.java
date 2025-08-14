3078243daf0efec6a7f6cd615385d24f674107ee
hawks314 <hawks314@naver.com>
2025-08-14 17:31:21 +0900

    feat: bootstrap Spring Boot project

---
+++ gpt-msa/src/main/java/com/hawks314/gpt_msa/GptMsaApplication.java
@@ -0,0 +1,13 @@
+package com.hawks314.gpt_msa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GptMsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GptMsaApplication.class, args);
+	}
+
+}
---
+++ gpt-msa/src/test/java/com/hawks314/gpt_msa/GptMsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hawks314.gpt_msa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GptMsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
